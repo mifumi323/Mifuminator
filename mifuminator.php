72191904750e09d9e5ff05d3861025374cfd2855
mifumi323 <mifumi323@gmail.com>
2015-11-18 23:56:53 +0900

    First Commit

---
+++ mifuminator.php
@@ -0,0 +1,22 @@
+<?php
+
+class Mifuminator {
+    private $db_file_path;
+    private $tmp_dir;
+
+    private $db;
+
+    public function __construct($db_file_path, $tmp_dir)
+    {
+        $this->db_file_path = $db_file_path;
+        $this->tmp_dir = $tmp_dir;
+        $this->db = new PDO('sqlite:'.$db_file_path);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->db->sqliteCreateFunction('RANDOM', 'mt_rand', 0);
+    }
+
+    public function getDB()
+    {
+        return $this->db;
+    }
+}
