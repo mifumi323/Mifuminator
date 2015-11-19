0da13535725da8d447df9e4814e878965901525b
mifumi323 <mifumi323@tgws.fromc.jp>
2015-11-19 20:36:27 +0900

    ログディレクトリもきっと必要だろう。

--- mifuminator.php
+++ mifuminator.php
@@ -3,13 +3,15 @@
 class Mifuminator {
     private $db_file_path;
     private $tmp_dir;
+    private $log_dir;
 
     private $db;
 
-    public function __construct($db_file_path, $tmp_dir)
+    public function __construct($db_file_path, $tmp_dir, $log_dir)
     {
         $this->db_file_path = $db_file_path;
         $this->tmp_dir = $tmp_dir;
+        $this->log_dir = $log_dir;
         $this->db = new PDO('sqlite:'.$db_file_path);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->db->sqliteCreateFunction('RANDOM', 'mt_rand', 0);
