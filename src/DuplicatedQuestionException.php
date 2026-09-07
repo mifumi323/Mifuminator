adda4bd09ce7eaddb36161258b5ec6fb09de4a8a
mifumi323 <mifumi323@gmail.com>
2026-09-07 20:14:57 +0900

    いらないンだよッ！丸投げコンストラクタなんか

--- src/DuplicatedQuestionException.php
+++ src/DuplicatedQuestionException.php
@@ -8,13 +8,4 @@ namespace Mifumi323\Mifuminator;
  */
 class DuplicatedQuestionException extends \Exception
 {
-    /**
-     * @param string|null $message エラーメッセージ
-     * @param int $code エラーコード
-     * @param \Exception|null $previous 前の例外
-     */
-    public function __construct($message = null, $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
--- src/GameInvalidTargetException.php
+++ src/GameInvalidTargetException.php
@@ -7,13 +7,4 @@ namespace Mifumi323\Mifuminator;
  */
 class GameInvalidTargetException extends \Exception
 {
-    /**
-     * @param string|null $message エラーメッセージ
-     * @param int $code エラーコード
-     * @param \Exception|null $previous 前の例外
-     */
-    public function __construct($message = null, $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
